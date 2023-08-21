40978b27788ec73afecfb99e3a88c144585ec281
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-08-21 17:23:43 +0330

    fix: folder structure
    added test anchors
    added gitkeep into some folders

--- src/app/main.cc
+++ src/app/main.cc
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <limits>
+#include <memory>
 
 int main(int argc, const char **argv) {
-  std::cout << "Hello world!" << std::endl;
+  auto min = std::numeric_limits<long long>::min();
+  std::cout << "limits (min) " << min << std::endl;
+
+  /// REVIEW[epic=test, seq=2] - add some more data
+  double numbers[] {0.2, 0.9, 2.4, 3};
+  for(auto num : numbers) {
+    std::cout
+      << std::boolalpha
+      << (static_cast<int>(num) > 0)
+      << std::endl;
+  }
+
+  auto ptr = std::make_shared<int>();
+  auto xptr = std::unique_ptr<std::string>();
+
+  /** FIXME[epic=test, seq=0] - test fixme
+      in this scenario we can handle much cases
+  */
+  /// LINK - src/app/CMakeLists.txt:11
+
+  /// NOTE - add more comments
+
   return 0;
 }
