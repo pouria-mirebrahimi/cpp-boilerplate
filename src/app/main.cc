21c3f523ff0e09e550c5600b7460db7c425935b3
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2025-02-23 16:06:47 +0000

    feat: added conan package manager

--- src/app/main.cc
+++ src/app/main.cc
@@ -2,17 +2,19 @@
 #include <limits>
 #include <memory>
 
-int main(int argc, const char **argv) {
+int main(int argc, const char **argv)
+{
   auto min = std::numeric_limits<long long>::min();
   std::cout << "limits (min) " << min << std::endl;
 
   /// REVIEW[epic=test, seq=2] - add some more data
-  double numbers[] {0.2, 0.9, 2.4, 3};
-  for(auto num : numbers) {
+  double numbers[]{0.2, 0.9, 2.4, 3};
+  for (auto num : numbers)
+  {
     std::cout
-      << std::boolalpha
-      << (static_cast<int>(num) > 0)
-      << std::endl;
+        << std::boolalpha
+        << (static_cast<int>(num) > 0)
+        << std::endl;
   }
 
   auto ptr = std::make_shared<int>();
